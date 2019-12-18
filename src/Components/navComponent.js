af25299d2a92d31439c77298cb169f95c62e7a34
צחי בן חמו <tzachi222@Gmail.com>
2019-12-18 15:42:55 +0200

    nav fix

--- src/Components/navComponent.js
+++ src/Components/navComponent.js
@@ -19,19 +19,19 @@ class NavComponent extends React.Component {
                 <nav id="nav">
                 <ul className={this.state.shouldHide ? 'fade out' : 'fade in'}>
                 <li title="עמוד הבית">
-                    <NavLink  exact  to="/" activeClassName="active">
+                    <NavLink  exact  to="/" activeClassName="active" onClick={() => this.toggleNav()}>
                         <span className="nav_text">ראשי</span>
                         <span className="icon fa-home"></span>
                     </NavLink>
                     </li>
-                    <li title="תיק עבודות">
-                        <NavLink to="/gallery"  activeClassName="active">
+                    <li title="תיק סעבודות">
+                        <NavLink to="/gallery"  activeClassName="active" onClick={() => this.toggleNav()}>
                         <span className="nav_text">תיק עבודות</span>
                         <span className="icon fa-camera-retro"></span>
                         </NavLink>
                     </li>
                     <li title="מי אנחנו">
-                        <NavLink to="/generic"  activeClassName="active">
+                        <NavLink to="/generic"  activeClassName="active" onClick={() => this.toggleNav()}>
                         <span className="nav_text">מי אנחנו</span>
                         <span className="icon fa-file-text-o"></span>
                         </NavLink>
